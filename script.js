39138b1ee165fa9f2262c7846867c44bd822d989
abhishek <abhi19982014@gmail.com>
2023-10-30 08:05:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const buttons = document.querySelectorAll('.button')
+
+
+const body = document.querySelector("body");
+
+buttons.forEach((button)=>{
+console.log(button);
+button.addEventListener('click',function(e){
+    console.log(e);
+console.log(e.target);
+if(e.target.id ==='grey'){
+    body.style.backgroundColor = 'white';
+}
+if(e.target.id ==='white'){
+    // body.style.backgroundColor = e.target.id;
+    body.style.backgroundColor = 'red';
+
+}
+if(e.target.id ==='blue'){
+    // body.style.backgroundColor = e.target.id;
+    body.style.backgroundColor = 'purple';
+
+}
+if(e.target.id ==='yellow'){
+    // body.style.backgroundColor = e.target.id;
+    body.style.backgroundColor = 'green';
+
+}
+if(e.target.id === 'red'){
+    // body.style.backgroundColor = e.target.id;
+    body.style.backgroundColor = 'pink';
+
+}
+
+})
+});
